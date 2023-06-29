96e5c32403b5d7949ec6d2993e962b96b1bd164b
CuteWaterBeary <original.vr.creator@gmail.com>
2023-06-29 16:06:22 -0700

    is there where version comes from?

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PACKAGES = {
     name: "cute.waterbeary.markdownviewer",
     displayName: "Unity Markdown Viewer",
     description: "Editor inspector for markdown files",
-    version: "2.0.0",
+    version: "3.0.0",
     author: {
       name: "CuteWaterBeary",
       url: "https://discord.gg/RjCae4XZCX",
